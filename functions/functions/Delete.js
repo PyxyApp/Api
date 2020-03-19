27b6c02571361899fba26720ae9ccb5afea14331
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2020-03-19 16:04:20 +0100

    update delete method

--- functions/functions/Delete.js
+++ functions/functions/Delete.js
@@ -11,51 +11,6 @@ module.exports = {
                 return res.status(500).send(errorMessage('', error.message));
             }
         })();
-    },
-
-    deleteLists: (req, res, db, errorMessage, Delete) => {
-        (async () => {
-            try {
-                const document = db.collection('users').doc(req.params.id)
-                    .collection('lists').doc(req.params.idList);
-                // Check if tasks
-                const query = document.collection("tasks");
-                let response = [];
-                await query.get().then(querySnapshot => {
-                    let docs = querySnapshot.docs;
-                    for (let doc of docs) {
-                        const selectedActivitiesByList = doc.data();
-                        response.push(selectedActivitiesByList);
-                    }
-                });
-                if(response.length > 0){
-                    for(let i=0; i< response.length; i++){
-                        req.params.idTask = response[i].id;
-                        Delete.deleteTask(req, res, db, errorMessage)
-                    }
-                }
-                await document.delete();
-                return res.status(200).send('The List was deleted with success and the tasks was also deleted !');
-            } catch (error) {
-                console.log(error.message);
-                return res.status(500).send(errorMessage('', error.message));
-            }
-        })();
-    },
-
-    deleteTask: (req, res, db, errorMessage) => {
-        (async () => {
-            try {
-                const document = db.collection('users').doc(req.params.id)
-                    .collection('lists').doc(req.params.idList)
-                    .collection('tasks').doc(req.params.idTask);
-                await document.delete();
-                return res.status(200).send('The Task was deleted with success !');
-            } catch (error) {
-                console.log(error);
-                return res.status(500).send(errorMessage('', error.message));
-            }
-        })();
     }
 
 };
