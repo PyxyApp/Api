087b9bf948d34ea2876d638a01ab09356b994ecd
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2020-04-07 17:39:47 +0200

    Fix update tasks : date_Done already update when tasks is_done

--- functions/functions/Update/updateTask.js
+++ functions/functions/Update/updateTask.js
@@ -2,13 +2,8 @@ module.exports = {
     updateTask: async (req, res, db) => {
         let date_done = null;
         if(req.body.is_done){
-            if(req.body.date_done === null){
                 date_done = new Date(Date.now())
-                console.log("here");
-            }
         }
-        console.log(req.body.date_done === null);
-        console.log(date_done);
         const document = db.collection(req.params.data).doc(req.params.id);
         await document.update({
             name: req.body.name,
