00de2a3fcc8b461e58e85a1971cef389143a9e88
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2020-04-07 18:20:47 +0200

    set missing data field

--- functions/functions/Update/updateUser.js
+++ functions/functions/Update/updateUser.js
@@ -5,20 +5,25 @@ module.exports = {
             emailVerified: req.body.email_verified,
             phoneNumber: req.body.phone,
             password: req.body.password,
-            displayName: req.body.displayName,
+            displayName: req.body.firstname + req.body.lastname,
             photoURL: req.body.photoURL,
-            disabled: req.body.disabled
+            disabled: req.body.is_active
         })
             .then((userRecord) => {
-                console.log(userRecord);
                 const document = db.collection(req.params.data).doc(req.params.id);
                     document.update({
-                    name: {
-                        firstname: req.body.firstname,
-                        lastname: req.body.lastname
-                    },
-                    nat: req.body.nat,
-                    phone: req.body.phone
+                        email: req.body.email,
+                        name: {
+                            firstname: req.body.firstname,
+                            lastname: req.body.lastname,
+                            username: req.body.username
+                        },
+                        acp: {
+                            admin: req.body.admin
+                        },
+                        nat: req.body.nat,
+                        phone: req.body.phone,
+                        is_active: req.body.is_active
                 });
             })
             .catch(e => {
